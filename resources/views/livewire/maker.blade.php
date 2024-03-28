b632ad76195d5943580bb5b41e9dd420a15b984a
Rafael Lunardelli <rafael@lunardelli.me>
2024-03-28 17:46:59 -0600

    showing data

--- resources/views/livewire/maker.blade.php
+++ resources/views/livewire/maker.blade.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Str;
 use App\Models\Url;
 use Illuminate\Database\Eloquent\Collection;
 use Livewire\Attributes\Computed;
+use App\Models\WebhookRequest;
 
 new class extends Component {
+    public ?WebhookRequest $webhookRequest = null;
+
     public function createNewWebhook(): void
     {
         $url = Url::query()->create([
@@ -19,7 +22,12 @@ new class extends Component {
     #[Computed]
     public function urls()
     {
-        return Url::all();
+        return Url::query()->with('requests')->get();
+    }
+
+    public function setR($id)
+    {
+        $this->webhookRequest = WebhookRequest::find($id);
     }
 
     public function getSlug()
@@ -57,7 +65,7 @@ new class extends Component {
     }
 }; ?>
 
-<div class="grid grid-cols-2 gap-4 h-full p-10">
+<div class="grid grid-cols-3 gap-4 h-full p-10">
     <div class="bg-slate-900 rounded-lg p-4">
         <button class="bg-yellow-500 rounded-lg shadow px-4 text-slate-900 hover:bg-opacity-80" wire:click="createNewWebhook">
             Novo webhook
@@ -65,16 +73,44 @@ new class extends Component {
 
         <ul class="overflow-y-auto h-[600px] text-lg flex flex-col space-y-2">
             @foreach($this->urls as $url)
-                <li class="flex justify-between">
-                    {{ route('handle-requests', $url) }}
-                    <a class="bg-yellow-500 rounded-lg shadow px-4 text-slate-900 hover:bg-opacity-80" href="{{ route('handle-requests', $url) }}?test=1" target="_blank">
-                        test
-                    </a>
+                <li>
+                    <div class="flex justify-between">
+                        {{ route('handle-requests', $url) }}
+                        <a class="bg-yellow-500 rounded-lg shadow px-4 text-slate-900 hover:bg-opacity-80" href="{{ route('handle-requests', $url) }}?test=1" target="_blank">
+                            test
+                        </a>
+                    </div>
+                    <ul>
+                        @foreach($url->requests as $request)
+                            <li class="cursor-pointer hover:text-yellow-300" wire:click="setR({{ $request->id }})">
+                                {{ $request->method }} :: {{ $request->created_at->diffForHumans() }}
+                            </li>
+                        @endforeach
+                    </ul>
                 </li>
             @endforeach
         </ul>
     </div>
-    <div class="bg-slate-900 rounded-lg p-4 overflow-y-auto">
-        kajsd
+    <div class="bg-slate-900 rounded-lg p-4 overflow-y-auto col-span-2">
+        @if($webhookRequest)
+            <div>
+                <div>{{ $webhookRequest->method }}</div>
+
+                <div class="bg-slate-800 opacity-90 rounded-lg p-2 text-base mt-4 font-bold">
+                    From IP {{ $webhookRequest->ip }} at {{ $webhookRequest->created_at->format('d, M h:i:s') }}
+                </div>
+
+                <div class="bg-slate-800 opacity-90 rounded-lg p-2 text-base mt-4 divide-y divide-amber-50 divide-opacity-20">
+                    @foreach($webhookRequest->headers as $header => $body)
+                        <div class="grid grid-cols-6 py-2">
+                            <div>{{ $header }}</div>
+                            <div class="col-span-4 text-wrap">{{ $body[0] }}</div>
+                        </div>
+                    @endforeach
+                </div>
+
+                <pre class="bg-slate-800 opacity-90 rounded-lg p-2 text-base mt-4">{{ json_encode(json_decode($webhookRequest->body),JSON_PRETTY_PRINT) }}</pre>
+            </div>
+        @endif
     </div>
 </div>
