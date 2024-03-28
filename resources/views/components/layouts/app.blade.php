35212bb7aa5aa36ad12e1bd8747e3d900a529d1a
Rafael Lunardelli <rafael@lunardelli.me>
2024-03-28 16:38:39 -0600

    instalando tailwind

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
-    </head>
-    <body>
-        {{ $slot }}
-    </body>
+    <title>{{ $title ?? 'Page Title' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-slate-800 text-slate-300">
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+{{ $slot }}
+</body>
 </html>
